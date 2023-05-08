aca9966138f9954d440f1d9226ddd3d1f19dba1a
Sandy Caba <joriel@Sandys-MacBook-Air.local>
2023-05-08 16:28:41 -0400

    added link to start investing button

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 import Analytics from './components/Analytics';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Hero from './components/Hero';
 import Nav from './components/Nav';
+import Signup from './components/Signup'
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Nav />
       <Hero />
       <Analytics />
       <Contact />
       <Footer />
-    </div>
+        <Signup />
+  </div>
   );
 }
 
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import Nav from './Nav';
 
 function Contact() {
   return (
@@ -10,8 +12,8 @@ function Contact() {
         </div>
         <div className=' my-4'>
           <div className=' flex flex-col sm:flex-row items-center justify-between w-full'>
-          <input className=' p-3 flex w-full rounded-md text-black' type="email" placeholder='Enter Email' />
-            <button className=' bg-[#00df9a] text-black rounded-md font-medium w-[200px] ml-4 my-6 px-6 py-3'>Notify Me</button>
+            <input className=' p-3 flex w-full rounded-md text-black' type="email" placeholder='Enter Email' />
+              <button className=' bg-[#00df9a] text-black rounded-md font-medium w-[200px] ml-4 my-6 px-6 py-3'>Notify Me</button>
           </div>
           <p>We care about the protection of your data. Read our <span className=' text-[#00df9a] underline cursor-pointer'>Privacy Policy</span>.</p>
         </div>
